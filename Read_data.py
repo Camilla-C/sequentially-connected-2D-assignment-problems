ba75c7b4466f4e9f14b30a8986f4c765b1b5810a
Camilla-C <81565126+Camilla-C@users.noreply.github.com>
2022-07-07 18:23:34 +0200

    Delete Read_data.py

--- Read_data.py
+++
@@ -1,8 +0,0 @@
-def Read_data(data):
-    
-    dataX = scipy.io.loadmat('X_' + data + '.mat')
-    dataY = scipy.io.loadmat('Y_' + data + '.mat')
-    X = dataX['temp']
-    Y = dataY['temp']
-    
-    return X, Y
